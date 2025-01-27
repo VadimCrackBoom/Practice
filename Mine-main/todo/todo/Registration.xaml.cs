9c27d32f4076f96a74a56b7611df7df50bc64fa9
Вадим <werty193842@gmail.com>
2025-01-27 21:36:19 +0300

    Add files via upload

---
+++ Mine-main/todo/todo/Registration.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using todo.Repository;
+using TodoEntities;
+
+namespace todo
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        public void RemoveText(object sender, EventArgs e)
+        {
+            TextBox instance = (TextBox)sender;
+            if (instance.Text == instance.Tag.ToString())
+                instance.Text = "";
+            instance.Opacity = 1;
+        }
+
+        public void AddText(object sender, EventArgs e)
+        {
+            TextBox instance = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(instance.Text))
+                instance.Text = instance.Tag.ToString();
+            instance.Opacity = 0.4;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn login = new LogIn();
+            login.Show();
+            this.Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var userRepo = new UserRepository();
+            bool success = userRepo.RegisterUser(nameTB.Text, emailTB.Text, passwordTB.Text);
+
+            string email = emailTB.Text;
+            string password = passwordTB.Text;
+            string name = nameTB.Text;
+
+            if (!name.ValidateName())
+            {
+                MessageBox.Show("Имя должно содержать 3 или более символов");
+                return;
+            }
+
+            if (!email.ValidateEmail())
+            {
+                // error
+                MessageBox.Show("Некорректная почта");
+                return;
+            }
+
+            if (!password.ValidatePassword())
+            {
+                MessageBox.Show("Пароль должен содержать 6 символов или более");
+                return;
+            }
+
+            if (passwordTB.Text != againPasswordTB.Text)
+            {
+                MessageBox.Show("Пароли должны совпадать");
+                return;
+            }
+
+            if (!success)
+            {
+                MessageBox.Show("Почта уже занята");
+                return;
+            }
+
+            if (email.ValidateEmail() && password.ValidatePassword() && name.ValidateName())
+                if (success)
+                {
+                    MessageBox.Show("Регистрация прошла успешно!");
+                }
+                else
+                {
+                    MessageBox.Show("Почта уже занята. Пожалуйста, введите другой адрес электронной почты");
+                }
+
+            LogIn logIn = new LogIn();
+            logIn.Show();
+            this.Hide();
+
+        }
+    }
+}
---
+++ Mine-main/todo/todo/Repository/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoEntities;
+
+namespace todo.Repository
+{
+    public class UserRepository
+    {
+        private static List<UserModel> _users = new List<UserModel>()
+        {
+        //new UserModel {Email = "werty211@gmail.com",  Name = "Андрей", Password = "241024102410"}
+        };
+        public static UserModel CurrentUser { get; private set; }
+        public UserModel GetUser(string email, string password)
+        {
+            return _users.FirstOrDefault(user => user.Email == email && user.Password == password);
+        }
+
+        public bool RegisterUser(string username, string useremail, string password)
+        {
+
+            if (_users.Exists(u => u.Email.Equals(useremail, StringComparison.OrdinalIgnoreCase)))
+            {
+                return false;
+            }
+
+            var newUser = new UserModel
+            {
+                Name = username,
+                Email = useremail,
+                Password = password
+            };
+            _users.Add(newUser);
+
+            //_users.Add( new UserModel { UserName = username, UserEmail = useremail, Password = password });
+
+            CurrentUser = newUser;
+
+            return true;
+        }
+
+        public static bool AuthenticateUser(string useremail, string username, string password)
+        {
+            CurrentUser = new UserModel();
+
+            var user1 = _users.FirstOrDefault(user => user.Email == useremail && user.Password == password);
+
+            if (user1 == null)
+            {
+
+                return false;
+            }
+
+
+            CurrentUser = user1;
+
+            return true;
+        }
+    }
+}
