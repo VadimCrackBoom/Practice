7f4179c62130d8f98b0fb53f3558e89d59f55a83
Вадим <werty193842@gmail.com>
2025-02-03 17:44:25 +0300

    Add files via upload

---
+++ todo/TodoEntities/UserModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TodoEntities
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
