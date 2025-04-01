93ac45b1c362ea4b573caee0c85652ae0a17ac86
Вадим <werty193842@gmail.com>
2025-04-01 17:31:38 +0300

    Add files via upload

---
+++ Practice-Lab_animations/todo/CompletedTasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo
+{
+    public class CompletedTasks
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
